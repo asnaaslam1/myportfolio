dcc1bb7754957fabedd3396be1d51e11059157f0
asnaaslam1 <asnaaslam91@gmail.com>
2026-02-27 22:32:48 +0530

    update html pg

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+const themeBtn = document.getElementById('theme-btn');
+const icon = themeBtn.querySelector('i');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');
+    icon.classList.replace('fa-moon', 'fa-sun');
 }
+
+themeBtn.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+        localStorage.setItem('theme', 'light');
+    }
+});
