50880d31ea9b285d5c05420a77fc972e25cf6e80
asnaaslam1 <asnaaslam91@gmail.com>
2026-02-27 22:36:32 +0530

    update js pg

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ const themeBtn = document.getElementById('theme-btn');
 const icon = themeBtn.querySelector('i');
 const body = document.body;
 
+// Check for saved user preference in local storage
 if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme');
-    icon.classList.replace('fa-moon', 'fa-sun');
+    enableDarkMode();
 }
 
 themeBtn.addEventListener('click', () => {
-    body.classList.toggle('dark-theme');
-
-    if (body.classList.contains('dark-theme')) {
-        icon.classList.replace('fa-moon', 'fa-sun');
-        localStorage.setItem('theme', 'dark');
+    const isDark = body.classList.contains('dark-theme');
+    
+    if (isDark) {
+        enableLightMode();
     } else {
-        icon.classList.replace('fa-sun', 'fa-moon');
-        localStorage.setItem('theme', 'light');
+        enableDarkMode();
     }
 });
+
+function enableDarkMode() {
+    body.classList.add('dark-theme');
+    icon.classList.replace('fa-moon', 'fa-sun'); // Change icon to sun
+    localStorage.setItem('theme', 'dark');
+}
+
+function enableLightMode() {
+    body.classList.remove('dark-theme');
+    icon.classList.replace('fa-sun', 'fa-moon'); // Change icon to moon
+    localStorage.setItem('theme', 'light');
+}
